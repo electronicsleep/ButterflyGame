5ee8e6570b30062fb4f97b1f2d2ab4fbe711a3d1
Chris Robertson <electronicsleep@gmail.com>
2022-12-01 08:19:25 -0800

    update-movement

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ butterfly.pos = 100, 100
 WIDTH = 800
 HEIGHT = 600
 
-print(f"Starting [red]ButterflyGame[/red]")
-print(f"Starting [green]Use Right/Left arrow keys[/green]")
+print(f"INFO: Starting [red]ButterflyGame[/red]")
+print(f"INFO: Starting [green]Use arrow keys to move butterfly[/green]")
+
 
 def draw():
+    print(f"[yellow]INFO: draw[/yellow]")
     screen.clear()
     butterfly.draw()
 
 
 def update():
-    if keyboard.right:
-        print(f"[green]right[/green]")
-        butterfly.right += 1
-    if keyboard.left:
-        print(f"[green]left[/green]")
+    print(f"[yellow]INFO: update[/yellow]")
+    if keyboard.up:
+        print(f"[green]INFO: up[/green]")
+        butterfly.top -= 1
+    elif keyboard.down:
+        print(f"[green]INFO: down[/green]")
+        butterfly.top += 1
+    elif keyboard.right:
+        print(f"[green]INFO: right[/green]")
+        butterfly.right += 2
+    elif keyboard.left:
+        print(f"[green]INFO: left[/green]")
         butterfly.left -= 1
-    butterfly.left += 1
+    else:
+        butterfly.left += 1
+
     if butterfly.left > WIDTH:
         butterfly.right = 0
