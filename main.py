4b65e73977db4f9cb93bf8af05e590ba289048df
Chris Robertson <electronicsleep@gmail.com>
2024-06-15 19:37:24 -0700

    adding-flower-background

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from rich import print
+
+background = Actor('background')
+background.pos = 0,0
+
+
 butterfly = Actor('butterfly')
 butterfly.pos = 100, 100
+
+flower = Actor('flower')
+flower.pos = 300, 200
 
 WIDTH = 800
 HEIGHT = 600
@@ -10,8 +18,11 @@ print(f"INFO: Starting [green]Use arrow keys to move butterfly[/green]")
 
 
 def draw():
+    screen.fill((128, 0, 0))
     print(f"[yellow]INFO: draw[/yellow]")
     screen.clear()
+    background.draw()
+    flower.draw()
     butterfly.draw()
 
 
@@ -19,18 +30,18 @@ def update():
     print(f"[yellow]INFO: update[/yellow]")
     if keyboard.up:
         print(f"[green]INFO: up[/green]")
-        butterfly.top -= 1
+        butterfly.top -= .5
     elif keyboard.down:
         print(f"[green]INFO: down[/green]")
-        butterfly.top += 1
+        butterfly.top += .5
     elif keyboard.right:
         print(f"[green]INFO: right[/green]")
-        butterfly.right += 2
+        butterfly.right += 1
     elif keyboard.left:
         print(f"[green]INFO: left[/green]")
-        butterfly.left -= 1
+        butterfly.left -= .5
     else:
-        butterfly.left += 1
+        butterfly.left += .5
 
     if butterfly.left > WIDTH:
         butterfly.right = 0
